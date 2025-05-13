4d080e789f1fa276cdb5649cb744c0292ed63e4a
LiquidApplesx <liquidapples@proton.me>
2025-05-12 22:03:01 -0400

    Initial flake setup

---
+++ configuration.nix
@@ -0,0 +1,127 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running 'nixos-help').
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with 'passwd'.
+  users.users.kari = {
+    isNormalUser = true;
+    description = "kari";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    wget
+    neovim
+    steam
+    kitty
+    fastfetch
+    firefox-devedition-bin
+    github-cli
+    # Note: The theming and Hyprland packages are moved to home-manager configuration
+  ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    liberation_ttf
+  ];
+
+  # DBus and XDG desktop portal
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  # Enable SDDM display manager and X server
+  services.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+
+  # Hyprland - now managed by the flake
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Environment variables
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1"; # Fix for electron apps in wayland
+  };
+
+  # Pipewire audio
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
+  };
+
+  # Enable flakes and nix-command
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken.
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "NixOS configuration with Hyprland and Catppuccin Mocha theme";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    hyprland.url = "github:hyprwm/Hyprland";
+    
+    # Optional: Catppuccin theme as a flake
+    catppuccin-nix = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, hyprland, catppuccin-nix, ... }@inputs:
+    let
+      system = "x86_64-linux"; # Adjust for your system architecture
+      pkgs = nixpkgs.legacyPackages.${system};
+      
+      # Import your hardware configuration
+      inherit (self) outputs;
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {  # Using 'nixos' as hostname from your config
+          inherit system;
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./configuration.nix
+            
+            # Import Hyprland NixOS module
+            hyprland.nixosModules.default
+            
+            # Home Manager module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs outputs; };
+              home-manager.users.kari = import ./home.nix;  # Using 'kari' as username from your config
+            }
+          ];
+        };
+      };
+    };
+}
